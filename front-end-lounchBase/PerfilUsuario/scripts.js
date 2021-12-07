8e6556b215e5d67e6847655633c2d74ffca7ff77
Wagner dos santos <47212801+wbatista985@users.noreply.github.com>
2021-12-06 21:14:39 -0300

    exercicios-lounchBase
    exercicios - perfil do usuario com display grid e javascript

---
+++ front-end-lounchBase/PerfilUsuario/scripts.js
@@ -0,0 +1,19 @@
+//modal do video
+const modalOverlay = document.querySelector('.modal-overlay');
+const cards = document.querySelectorAll('.card');
+
+//clicando no card da  player no video de acordo com o Id do video no card
+for (let card of cards) {
+    card.addEventListener("click", function(){
+        const videoId = card.getAttribute("id");
+        modalOverlay.classList.add('active')
+        modalOverlay.querySelector("iframe").src = `https://www.youtube.com/embed/${videoId}`
+    })
+}
+
+// remove video e fecha
+document.querySelector(".close-modal").addEventListener("click", function(){
+    modalOverlay.classList.remove("active")
+    modalOverlay.querySelector("iframe").src = ""
+})
+
